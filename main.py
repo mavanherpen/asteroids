fb81e92efc472ee8a3e301c8ddb77b87fc1bdeb7
mavanherpen <m.a.vanherpen@gmail.com>
2024-12-13 14:39:12 +0100

    Adding player rotation left and right

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ def main():
         
         pygame.Surface.fill(screen, (0,0,0))
         
+        player.update(dt)
+
         player.draw(screen)
 
         pygame.display.flip()
--- player.py
+++ player.py
@@ -19,3 +19,16 @@ class Player(CircleShape):
     # overriding parent draw method
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    # adding rotate method
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    # adding update of movement method
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
