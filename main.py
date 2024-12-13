6ba5edbaac2f5d6ebd4830f585746eb2695f11f6
mavanherpen <m.a.vanherpen@gmail.com>
2024-12-13 14:05:41 +0100

    Limiting game to 60FPS

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,8 +18,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
